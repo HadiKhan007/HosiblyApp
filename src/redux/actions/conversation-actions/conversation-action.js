3ea68852afec623ee9c37b76b5828da4a1109b85
HamzaZahid5 <109659559+HamzaZahid5@users.noreply.github.com>
2022-09-06 17:36:38 +0500

    ChatInProgress

---
+++ src/redux/actions/conversation-actions/conversation-action.js
@@ -0,0 +1,90 @@
+import * as TYPES from '../types';
+
+export const send_FCM_Request = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.SEND_FCM_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const createConversationRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.CREATE_CONVERSATION_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const getconversationListRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.GET_CONVERSATION_LIST_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const deleteConversationRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.DELETE_CONVERSATION_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const getAllMessagesRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.GET_ALL_MESSAGES_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const readMessagesRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.READ_MESSAGES_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const reportUserRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.REPORT_USER_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const blockUserRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.BLOCK_USER_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const getBlockUserListRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.GET_BLOCK_USER_LIST_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const unBlockUserRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.UNBLOCK_USER_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
+export const getNotificationListRequest = (params, cbSuccess, cbFailure) => {
+  return {
+    type: TYPES.GET_NOTIFICATION_LIST_REQUEST,
+    params,
+    cbSuccess,
+    cbFailure,
+  };
+};
